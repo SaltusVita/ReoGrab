937453be118eab0d4d40c69ad4f5b446059f71b2
garet <garet@mdh.su>
2016-07-09 23:04:39 +0300

    Add some funcs

--- Aliexpress.py
+++ Aliexpress.py
@@ -2,8 +2,31 @@ from Spider import *
 
 class AliExpress(Spider):
     
+    def __init__(self):
+        self._routes =  [
+            {
+                'name': 'Category',
+                're': '[^a]+?aliexpress.com/af/category/.+?'
+            },
+            {
+                'name': 'Item',
+                're': '[^a]+?aliexpress.com/item/.+?'
+            },
+            {
+                'name': 'AllCategory',
+                're': '[^a]+?aliexpress.com/all-wholesale-products.html'
+            },
+        ]
+    
+    def Run(self):
+        url = 'http://ru.aliexpress.com/af/category/202001195.html'
+        route = self.Routing(url)
+        getattr(self, route['name'])()
+        #page = self.Download(url)
+        print(route)
+    
     def Category(self, page):
-        urls = page.find('.list-item a.product').href()
+        urls = page.find_all('.list-item a.product').href()
         self.add_urls(urls, page.url)
     
     def Item(self, page):
@@ -13,3 +36,11 @@ class AliExpress(Spider):
         item.img = page.find('.ui-image-viewer-thumb-frame img').src()
         item.category = page.category 
         self.Save(item, 'Items')
+    
+    def AllCategory(self, page):
+        urls = page.find_all('.cg-main a').href()
+        self.add_urls(urls, page.url)
+        
+
+bot = AliExpress()
+bot.Run()
--- Spider.py
+++ Spider.py
@@ -4,15 +4,23 @@ Created on 7/07/2016
 @author: garet
 '''
 
+import urllib.request
+import queue
+import re
+
 class Spider():
     
     def __init__(self):
         pass
     
     def Routing(self, url):
-        pass
+        for route in self._routes:
+            if re.match(route['re'], url) == None:
+                return route
+        return None
     
-    def Run(self):
-        pass
-    
-    
+    def Download(self, url):
+        request = urllib.request.urlopen(url)
+        page = request.read()
+        return page.decode('utf-8')
+        
