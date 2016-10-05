1bee008f34094150caf7cda7c03f96d4dee271e8
garet <garet@mdh.su>
2016-10-05 20:19:11 +0300

    Some fixes

--- Spiders.py
+++ Spiders.py
@@ -8,6 +8,7 @@ import urllib.request
 import queue
 import sqlite3
 import re
+import json
 from urllib.parse import urlparse
 
 from Parser import HtmlPage
@@ -16,9 +17,18 @@ class BaseSpider:
     
     def __init__(self):
         self.urls = QueueUrls()
+        self.cache = SqliteCache('some_db')
 
     def add_urls(self, urls):
         self.urls.add_urls(urls)
+
+    def add_urls_routed(self, urls):
+        result = []
+        for url in urls:
+            if self.fetch_route(url) is not None:
+                result.append(url)
+        #print(result)
+        self.add_urls(result)
 
     def add_routes(self, routes):
         pass
@@ -27,7 +37,8 @@ class BaseSpider:
         if not hasattr(self, 'routes'):
             return
         for route in self.routes:
-            if re.match(route['re'], url) is not None:
+            part_url = re.match(route['re'], url)
+            if part_url is not None and part_url.group(0) == url:
                 if 'skip' in route and route['skip'] is True:
                     break
                 return route
@@ -42,7 +53,7 @@ class BaseSpider:
     def run(self):
         self.init()
         self.work()
-        self.clear()
+        #self.clear()
 
     def init(self):
         if hasattr(self, 'start_urls'):
@@ -61,10 +72,43 @@ class BaseSpider:
                 getattr(self, route['name'])(response)
         pass
 
-    @staticmethod
-    def get_page(url):
-        r = urllib.request.urlopen(url)
+    def charset(self, headers):
+        encode = 'UTF-8'
+        if hasattr(headers, 'Content-Type'):
+            m = re.search('charset=([a-z 0-9\-\_]+)', self.headers, re.IGNORECASE)
+            if m:
+                encode = m.group(1)
+        return encode
+
+    def get_page(self, url):
+        r = self.cache.get(url)
+        if r is not None:
+            print(r['url'])
+            return Response(r)
+        r = self.get_data(url)
+        self.cache.set(r)
+        print('{0} --- {1}'.format(url, r['url']))
         return Response(r)
+
+    @staticmethod
+    def get_data(url):
+        try:
+            r = urllib.request.urlopen(url)
+            out = {
+                'url': r.geturl(),
+                'code': r.getcode(),
+                'headers': json.dumps(r.getheaders()),
+                'data': r.read()
+            }
+            return out
+        except urllib.error.HTTPError as e:
+            out = {
+                'url': e.geturl(),
+                'code': e.getcode(),
+                'headers': json.dumps(e.getheaders()),
+                'data': e.read()
+            }
+            return out
 
 
 class QueueUrls:
@@ -76,6 +120,8 @@ class QueueUrls:
     def add_url(self, url):
         u = urlparse(url)
         url = u[0] + '://' + u[1] + u[2] + u[3]
+        if u[4] != '':
+            url += '?' + u[4]
         if url not in self._urls_set:
             self._urls_queue.put(url)
             self._urls_set.add(url)
@@ -112,11 +158,12 @@ class SqliteCache:
         self._cursor = self._db.cursor()
         # Create table
         sql = """
-            CREATE TABLE IF NOT EXISTS tbl_urls
-            ( 
-                url text primary key not null, 
-                html text, 
-                time timestamp DEFAULT CURRENT_TIMESTAMP
+            CREATE TABLE IF NOT EXISTS tbl_urls(
+                url TEXT primary key not null,
+                code INTEGER,
+                headers TEXT,
+                data BLOB,
+                time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );"""
         self._cursor.execute(sql)
 
@@ -125,17 +172,26 @@ class SqliteCache:
             self.InitDB()
         sql = "SELECT * FROM tbl_urls WHERE url=?;"
         self._cursor.execute(sql, (url,))
-        return self._cursor.fetchone()
+        row = self._cursor.fetchone()
+        if row is not None:
+            out = {
+                'url': row[0],
+                'code': row[1],
+                'headers': json.loads(row[2]),
+                'data': row[3]
+            }
+            return out
+        return None
 
-    def set(self, url, data):
+    def set(self, dat):
         if self._cursor is None:
             self.init_db()
-        sql = "INSERT OR REPLACE INTO tbl_urls(url,html) VALUES (?,?);"
-        self._cursor.execute(sql, (url, data))
+        sql = "INSERT OR REPLACE INTO tbl_urls(url,code,headers,data) VALUES (?,?,?,?);"
+        self._cursor.execute(sql, (dat['url'], dat['code'], dat['headers'], dat['data']))
         self._db.commit()
 
 
-class Request:
+class Download:
 
     def __init__(self):
         self.method = 'GET'
@@ -145,15 +201,23 @@ class Request:
     def random_user_agent(self, browser=None, os=None):
         return 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 8.0; WOW64; Trident/5.0; .NET CLR 2.7.40781; .NET4.0E; en-SG)'
 
+    @staticmethod
+    def get_page(url):
+        r = urllib.request.urlopen(url)
+        code = r.getcode()
+        headers = r.getheaders()
+        data = r.read()
+        url = r.geturl()
+        #return Response(r)
+
 
 class Response:
 
     def __init__(self, res):
-        self._response = res
-        self.code = res.getcode()
-        self.headers = res.getheaders()
-        self.data = res.read()
-        self.url = res.geturl()
+        self.code = res['code']
+        self.headers = res['headers']
+        self.data = res['data']
+        self.url = res['url']
 
     def charset(self):
         encode = 'UTF-8'
--- fl.ru.py
+++ fl.ru.py
@@ -3,27 +3,46 @@ from Spiders import BaseSpider
 
 class FlRu(BaseSpider):
 
-    start_urls = 'https://www.fl.ru/freelancers/#34234'
+    #start_urls = 'https://www.fl.ru/users/i3qqq'  #'https://www.fl.ru/freelancers/#34234'
+    start_urls = 'https://www.fl.ru/users/i3qqq/'
     routes = [
-        #{
-        #    'name': 'user',
-        #    're': '[^f]*?fl.ru/users/.*?',
-        #},
         {
             'name': 'project',
-            're': '[^f]*?fl.ru/projects/.*?'
+            're': '[^f]*?fl.ru/users/[^/]*/viewproj.php\?prjid\=.*'
+        },
+        {
+            'name': 'user',
+            're': '[^f]*?fl.ru/users/[^/]*/',
+        },
+        {
+            'name': 'category',
+            're': '[^f]*?fl.ru/freelancers/.*'
         },
         {
             'name': 'main_page',
-            're': '[^f]*?fl.ru.*?',
+            're': '[^f]*?fl.ru/',
         },
     ]
 
     def main_page(self, request):
         p = request.parser()
-        print(request.url)
-        users = p.FindAll('.freelancer-name').Href()
-        self.add_urls(users)
+        links = p.FindAll('a').Href()
+        self.add_urls_routed(links)
+
+    def user(self, request):
+        p = request.parser()
+        links = p.FindAll('a').Href()
+        self.add_urls_routed(links)
+
+    def category(self, request):
+        p = request.parser()
+        links = p.FindAll('a').Href()
+        self.add_urls_routed(links)
+
+    def project(self, request):
+        p = request.parser()
+        links = p.FindAll('a').Href()
+        self.add_urls_routed(links)
 
 
 bot = FlRu()
